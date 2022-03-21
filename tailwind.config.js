193970937596841d890659522fcb1d3134477095
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2022-03-21 11:55:24 +0100

    Init Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.jsx"],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+}
