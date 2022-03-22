363a17fbd33b8bc5c2941227fe7d8d741cd38b2c
fabian222222 <fabian.zuo@supinternet.fr>
2022-03-22 15:49:21 +0100

    message page

--- src/Components/Chat/MessageSingle.jsx
+++ src/Components/Chat/MessageSingle.jsx
@@ -1,37 +1,33 @@
 import React from "react";
 import image from "../../Assets/Images/Lindsey.png";
 
-function MessageContainer({ sender = "you" }) {
+function MessageContainer({ message, name, date }) {
   return (
     <div className="p-5 mb-5">
       <div
         className={`flex items-center mb-4 ${
-          sender === "you" ? "justify-end" : "justify-start"
+          name === "You" ? "justify-end" : "justify-start"
         }`}
       >
         <div className="mr-2 rounded-full overflow-hidden w-5">
           <img className="w-full h-full object-contain" src={image} alt="" />
         </div>
-        <p className="text-[14px] mr-2">Lindsey Stroud</p>
-        <p className="text-[11px]">11:12</p>
+        <p className="text-[14px] mr-2">{name}</p>
+        <p className="text-[11px]">{date}</p>
       </div>
       <div
         className={`w-full flex ${
-          sender === "you" ? "justify-end" : "justify-start"
+          name === "You" ? "justify-end" : "justify-start"
         }`}
       >
         <p
-          className={`w-2/3 p-5 ${
-            sender === "you"
-              ? "text-black bg-slate-50 border-2 border-current rounded-tl-md rounded-br-md rounded-bl-md"
-              : "bg-sky-600 text-slate-50 rounded-tr-md rounded-br-md rounded-bl-md"
+          className={`w-[85%] p-5 ${
+            name === "You"
+              ? "text-black bg-white border-2 border-gray-200 rounded-tl-md rounded-br-md rounded-bl-md"
+              : "bg-sky-500 text-slate-50 rounded-tr-md rounded-br-md rounded-bl-md"
           } `}
         >
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh
-          mauris cursus mattis molestie. Ligula ullamcorper malesuada proin
-          libero nunc consequat interdum. A lacus vestibulum sed arcu non odio
-          euismod lacinia. Aliquet eget sit amet tellus cras adipiscing enim.
+          {message}
         </p>
       </div>
     </div>
--- src/Components/Chat/UserCard.jsx
+++ src/Components/Chat/UserCard.jsx
@@ -6,35 +6,69 @@ function UserCard({
   date,
   lastMessage,
   notif,
-  noRead,
   success,
   error,
   pending,
+  open,
+  sender,
 }) {
   return (
-    <div className="flex p-4 w-fit min-w-400px border-b-2 border-t-stone-600">
-      <div className="mr-2 rounded-full overflow-hidden w-8 h-8">
-        <img className="w-full h-full object-contain" src={userImage} alt="" />
+    <div
+      className={`flex p-4 w-fit min-w-400px border-b-2 border-t-stone-600 bg-white ${
+        open ? "border-r-2 border-r-sky-500" : ""
+      }`}
+    >
+      <div
+        className={`mr-2 rounded-full overflow-hidden w-8 h-8 ${
+          open ? "border-sky-500 border-2" : ""
+        }`}
+      >
+        <img
+          className="w-full h-full object-cover"
+          src={`../../src/Assets/Images/${userImage}.png`}
+          alt={userImage}
+        />
       </div>
       <div className="w-full">
         <div className="flex justify-between items-center mb-2">
-          <p>{name}</p>
+          <p className={`${open ? "text-sky-500" : "text-black"}`}>{name}</p>
           <p className="text-[12px] text-stone-400">{date}</p>
         </div>
         <div className="flex justify-between align-center">
-          <p className="text-stone-400">{lastMessage} </p>
+          <p className="text-stone-400 text-xs">
+            {sender === "you" ? `You: ${lastMessage}` : lastMessage}{" "}
+          </p>
           {notif && (
             <div className="w-6 h-6 rounded-md flex justify-center items-center bg-blue-500 text-slate-100">
-              <p className="text-xs">1</p>
+              <p className="text-xs">{notif}</p>
             </div>
           )}
-          {noRead && <p>{noRead}</p>}
           {success && (
-            <img src="./Assets/Images/success.svg" alt="success logo" />
+            <div className="w-5 h-4-5">
+              <img
+                className="w-full object-cover"
+                src="../../src/Assets/Images/success.png"
+                alt="success logo"
+              />
+            </div>
           )}
-          {error && <img src="./Assets/Images/error.svg" alt="error logo" />}
+          {error && (
+            <div className="w-5 h-4-5">
+              <img
+                className="w-full object-cover"
+                src="../../src/Assets/Images/error.png"
+                alt="success logo"
+              />
+            </div>
+          )}
           {pending && (
-            <img src="./Assets/Images/pending.svg" alt="pending logo" />
+            <div className="w-5 h-4-5">
+              <img
+                className="w-full object-cover"
+                src="../../src/Assets/Images/pending.png"
+                alt="success logo"
+              />
+            </div>
           )}
         </div>
       </div>
