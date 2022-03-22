4f966a80740b941eb1f7690b081f0cca88c69112
Ulysse <ulysse.asso-o-emane@supinternet.fr>
2022-03-22 01:07:00 +0100

    Stash files

--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import CompleteHeader from "../Components/CompleteHeader";
 
 function Dashboard() {
-    return (
-        <div className="flex h-screen">
-            <CompleteHeader searchContent="Global Search">
-                <div>
-                    <p>hahah</p>
-                </div>
-            </CompleteHeader>
+  return (
+    <div className="flex h-screen">
+      <CompleteHeader searchContent="Global Search">
+        <div>
+          <p>hahah</p>
         </div>
-    );
+      </CompleteHeader>
+    </div>
+  );
 }
 
 export default Dashboard;
